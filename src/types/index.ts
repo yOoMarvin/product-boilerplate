8e45af68d5e17d4ea07c14c8b63e0c23ad7c78b4
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-01-06 19:12:36 +0100

    initial config and typescript support

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+// Export your types here e.g.
+//   export interface Bookmark {
+//     url: string;
+//     author: string | void;
+//     creator: string | void;
+//     description: string | void;
+//     image: string | void;
+//     site_name: string | void;
+//     title: string | void;
+//     host: string | void;
+//   }
