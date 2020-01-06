d21c702b36892c4a47bdcd92c89da30621ed7c5c
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2020-01-06 18:59:19 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  experimental: {
+    granularChunks: true,
+    modern: true
+  },
+  webpack: config => {
+    config.resolve.alias["~"] = path.resolve("./src");
+    return config;
+  }
+};
